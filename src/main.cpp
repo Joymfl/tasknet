44808afe175fcfcbf2b14d808979c447f8cb69f6
darkus <jmjoymallik@gmail.com>
2025-11-20 16:38:05 +0530

    Add basic win32 window creation | clang tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMSG, WPARAM wparam, LPARAM lparam) {
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMSG, WPARAM wparam,
+                            LPARAM lparam) {
   switch (uMSG) {
   case WM_DESTROY:
     PostQuitMessage(0);
@@ -10,7 +11,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMSG, WPARAM wparam, LPARAM lparam) 
     PAINTSTRUCT ps;
     HDC hdc = BeginPaint(hwnd, &ps);
 
-    const char* text = "Tasknet Core UI";
+    const char *text = "Tasknet Core UI";
     TextOutA(hdc, 10, 10, text, lstrlenA(text));
 
     EndPaint(hwnd, &ps);
@@ -23,11 +24,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMSG, WPARAM wparam, LPARAM lparam) 
   return DefWindowProc(hwnd, uMSG, wparam, lparam);
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-  (void) hPrevInstance;
-  (void) lpCmdLine;
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                     LPSTR lpCmdLine, int nCmdShow) {
+  (void)hPrevInstance;
+  (void)lpCmdLine;
 
-  const char ClASS_NAME[] = "Taskent window class";
+  constexpr char ClASS_NAME[] = "Taskent window class";
 
   WNDCLASSA wc = {};
   wc.lpfnWndProc = WindowProc;
@@ -37,11 +39,14 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
   wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 
   if (!RegisterClassA(&wc)) {
-    MessageBoxA(nullptr, "Failed to register window class.", "Error", MB_ICONERROR);
+    MessageBoxA(nullptr, "Failed to register window class.", "Error",
+                MB_ICONERROR);
     return 1;
   }
 
-  HWND hwnd = CreateWindowExA(0, ClASS_NAME, "Tasknet", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, nullptr, nullptr, hInstance, nullptr);
+  HWND hwnd = CreateWindowExA(0, ClASS_NAME, "Tasknet", WS_OVERLAPPEDWINDOW,
+                              CW_USEDEFAULT, CW_USEDEFAULT, 400, 400, nullptr,
+                              nullptr, hInstance, nullptr);
 
   if (hwnd == nullptr) {
     MessageBoxA(nullptr, "Failed to create window.", "Error", MB_ICONERROR);
@@ -52,7 +57,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
   UpdateWindow(hwnd);
 
   MSG msg = {};
-  while (GetMessage(&msg, nullptr, 0,0) > 0) {
+  while (GetMessage(&msg, nullptr, 0, 0) > 0) {
     TranslateMessage(&msg);
     DispatchMessage(&msg);
   }
